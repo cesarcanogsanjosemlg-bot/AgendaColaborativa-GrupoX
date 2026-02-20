799d00fac6f3ce7f82902fe03f7cacfe7df64fe0
ricardo <ricardo@DESKTOP-KLL37QO>
2026-02-20 09:28:40 +0100

    Commit con el atributo ArrayList de listaContactos, constructor con dicho atributo, y 2 métodos implementados, el primero eliminarContacto y el segundo buscarContacto que devuelve o el contacto buscado si existe y si no existe devuelve nulo, el primer método es para eliminar un contacto y el segundo método para buscar un contacto

--- AgendaColaborativa/src/package1/Agenda.java
+++ AgendaColaborativa/src/package1/Agenda.java
@@ -1,12 +1,38 @@
 package package1;
 
-public class Agenda {
+import java.util.ArrayList;
+import java.util.List;
 
+import controlador.Contacto;
+
+public class Agenda {
+	 private List<Contacto> listaContactos;
+
+	    public Agenda() {
+	        this.listaContactos = new ArrayList<>();
+	    }
 	public void agregarContacto(Contacto c) {}
 	
-	public void eliminarContacto(String nombre) {}
-	
-	public void buscarContacto(String nombre) {}
+	public void eliminarContacto(String nombre) {
+        Contacto c = buscarContacto(nombre);
+        if (c != null) { 
+            listaContactos.remove(c);
+            System.out.println("Contacto '" + nombre + "' eliminado correctamente.");
+        } 
+        else {
+            System.out.println("el contacto '" + nombre + "' no existe.");
+        }
+    }
+
+    public Contacto buscarContacto(String nombre) {
+        for (Contacto c : listaContactos) {
+            if (c.getNombre().equalsIgnoreCase(nombre)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
 	
 	public void listarContactos() {}
 	
